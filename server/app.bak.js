ad779f548c6dfbbbe3d2d2450c43d512b9cf3473
Carol1992 <1689597938@qq.com>
2017-10-21 10:59:11 +0800

    database

--- server/app.bak.js
+++ server/app.bak.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//  用户注册、登录、修改信息的接口路由
-var register = require('./routes/register') 
+
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
+
+view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// 用户注册、登录、修改信息的接口路由
-app.use('/register', register)
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
